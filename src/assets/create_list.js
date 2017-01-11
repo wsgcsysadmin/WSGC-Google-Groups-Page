b99cc9149e279f8d6cc6fcb4aceb8b347712c645
Kyla Harper <kyla.seraphine@gmail.com>
2017-01-11 14:38:13 -0600

    Now generates an html file

--- src/assets/create_list.js
+++ src/assets/create_list.js
@@ -1,5 +1,5 @@
 var options = {
-    valueNames: [ 'name', 'born' ]
+    valueNames: [ 'name', 'email', 'owners' ]
 };
 
-var userList = new List('users', options);
+var userList = new List('groups', options);
--- src/src/main.py
+++ src/src/main.py
@@ -1,16 +1,19 @@
 import secrets # You'll need to create your own secrets file
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 from googleDirectory import Groups, Members, User
 from googleapiclient.errors import HttpError
 
 if __name__ == '__main__':
     # Gather Google Data
+    print("Scraping data from Google...")
     groups_handler = Groups(secrets.domain)
     member_handler = Members()
     user_handler = User()
 
     groups = groups_handler.get_domain_groups()
     for group in groups:
+        print('\tProcessing: ' + group['name'])
         owners = member_handler.find_owners(group['id'])
         group['owners'] = []
         if owners:
@@ -24,5 +27,57 @@ if __name__ == '__main__':
                     'email': member['email']})
 
     # Generate an HTML template
-    # Run Flask to host page
+    print("Generating HTML content...")
+    html = "{% extends 'base.html' %}"
+    html += "{% block content_block %}"
+    for group in groups:
+        row_data = "<tr>"
 
+        row_data += '<td class="name">'
+        row_data += group['name']
+        row_data += '</td>'
+
+        row_data += '<td class="email">'
+        row_data += '<a href=mailto:"' + group['email'] + '">'
+        row_data += group['email']
+        row_data += '</a>'
+        row_data += '</td>'
+
+        row_data += '<td class="owners">'
+        owners = group['owners']
+        row_data += "<ul>"
+        if owners:
+            for member in owners:
+                row_data += "<li>"
+                name = member['name']
+                email = member['email']
+                row_data += '<a href="' + email + '">'
+                if name:
+                    row_data += name
+                else:
+                    row_data += email
+                row_data += '</a>'
+                row_data += "</li>"
+        else:
+            row_data += "<li>"
+            row_data += "No owners"
+            row_data += "</li>"
+        row_data += "</ul>"
+        row_data += '</td>'
+
+        row_data += "</tr>"
+        html += row_data
+
+    html += "{% endblock content_block %}"
+
+    env = Environment(
+        loader = FileSystemLoader('../assets'),
+        autoescape = select_autoescape(['html', 'xml'])
+    )
+
+    index_contents = env.from_string(html)
+    with open('../assets/index.html', 'w') as index_file:
+        index_file.write(index_contents.render())
+
+
+    # Run Flask to host page
